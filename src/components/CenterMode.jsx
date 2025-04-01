5d8caa5edfbb16acc86d8afb2d622082cce7562d
Sam-Haile <92647194+Sam-Haile@users.noreply.github.com>
2025-04-01 18:08:10 -0400

    working on recommended section

--- src/components/CenterMode.jsx
+++ src/components/CenterMode.jsx
@@ -28,7 +28,7 @@ function CenterMode({ games = [] }) {
           <div key={game.id} className="px-2">
             <div className="flex flex-cols-2 h-auto">
               <div className="bg-black w-1/2">
-              {game.heroes?.url ? (
+                {game.heroes?.url ? (
                   <img
                     src={game.heroes.url}
                     alt={`${game.name} hero`}
@@ -38,10 +38,39 @@ function CenterMode({ games = [] }) {
                   <div className="text-white p-2">No hero image found</div>
                 )}
 
-           </div>
+{/* <img 
+                  src={game.coverUrl}
+                  alt={`${game.name} cover`}
+                  className="w-auto h-24 bg-red-500 shadow-lg"
+                /> */}
+
+
+                <p className="pt-2 text-sm">
+                  {game.genres?.length > 0
+                    ? game.genres.map((genre) => genre).join(", ")
+                    : "No genres available"}
+                </p>
+
+                <p className="font-light text-sm">
+                  {game.platforms?.length > 0
+                    ? game.platforms.map((platform) => platform).join(", ")
+                    : "No platforms avaliable"}
+                </p>
+
+                <p className="line-clamp-3 pt-2 text-md font-light">
+                  {game?.storyline}
+                </p>
+                {/* 
+                <img 
+                  src={game.coverUrl}
+                  alt={`${game.name} cover`}
+                  className="absolute top-20 ml-12 w-[100px]"
+                /> */}
+
+              </div>
 
               <div className="w-1/2">
-                v
+               
               </div>
             </div>
           </div>
